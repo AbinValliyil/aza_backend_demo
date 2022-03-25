dec56c5ed63e7c8ec8c78e9f3346c43a21a0104b
Abin_Michael <91622979+AbinValliyil@users.noreply.github.com>
2022-03-25 10:31:01 +0530

    Update Order.py

--- Routers/Order.py
+++ Routers/Order.py
@@ -13,23 +13,26 @@ db = SessionLocal()
 
 @router.post('/Create_Order',tags=['USER_ORDER'])
 def create_user_Order( package: schemas.Package_Create,Pickup_address:schemas.Pickup_Address_Create,Delivery_address:schemas.Delivery_Address_Create,Delivery_details:schemas.Delivery_Details_Create, user_id:str,db: session = Depends(get_db)):
-    db_Package = models.Package(**package.dict(), owner_id=user_id)
-    db.add(db_Package)
-    db.commit()
-    db.refresh(db_Package)
-    db_Pickup_Address= models.Pickup_Address(**Pickup_address.dict(), owner_id=user_id)
-    db.add( db_Pickup_Address)
-    db.commit()
-    db.refresh(db_Pickup_Address)
-    db_Delivery_address= models.Delivery_Address(**Delivery_address.dict(), owner_id=user_id)
-    db.add( db_Delivery_address)
-    db.commit()
-    db.refresh( db_Delivery_address)
-    db_Delivery_details= models.Delivery_Details(**Delivery_details.dict(), owner_id=user_id)
-    db.add(db_Delivery_details)
-    db.commit()
-    db.refresh(db_Delivery_details)
-    return  str(uuid4())+'->'+str(user_id) + ','+"AZA"
+    try:
+        db_Package = models.Package(**package.dict(), owner_id=user_id)
+        db.add(db_Package)
+        db.commit()
+        db.refresh(db_Package)
+        db_Pickup_Address= models.Pickup_Address(**Pickup_address.dict(), owner_id=user_id)
+        db.add( db_Pickup_Address)
+        db.commit()
+        db.refresh(db_Pickup_Address)
+        db_Delivery_address= models.Delivery_Address(**Delivery_address.dict(), owner_id=user_id)
+        db.add( db_Delivery_address)
+        db.commit()
+        db.refresh( db_Delivery_address)
+        db_Delivery_details= models.Delivery_Details(**Delivery_details.dict(), owner_id=user_id)
+        db.add(db_Delivery_details)
+        db.commit()
+        db.refresh(db_Delivery_details)
+    except:
+           return {"status":404}
+    return  {'id':str(uuid4())+'->'+str(user_id) + ','+"AZA",'status':200}
    
 
 
